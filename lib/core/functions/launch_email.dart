6cb8baa9d9ecb7bb13974f845ac9633a7bf153c5
EsoElzantot <eslamelzantot@gmail.com>
2026-04-25 19:25:49 +0300

    solve email bug

--- lib/core/functions/launch_email.dart
+++ lib/core/functions/launch_email.dart
@@ -1,7 +1,4 @@
-import 'dart:io' show Platform;
-
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> sendMail({
@@ -10,72 +7,30 @@ Future<void> sendMail({
   required String body,
   required BuildContext context,
 }) async {
-  final String encodedEmail = Uri.encodeComponent(email);
-  final String encodedSubject = Uri.encodeComponent(subject);
-  final String encodedBody = Uri.encodeComponent(body);
+  final encodedSubject = Uri.encodeComponent(subject);
+  final encodedBody = Uri.encodeComponent(body);
 
-  if (kIsWeb) {
-    // ✅ لو بيفتح على موبايل browser - يجرب يفتح Gmail App الأول
-    final Uri gmailAppUri = Uri.parse(
-      'googlegmail://co?to=$encodedEmail&subject=$encodedSubject&body=$encodedBody',
-    );
+  final gmailApp = Uri.parse(
+    'googlegmail://co?to=$email&subject=$encodedSubject&body=$encodedBody',
+  );
 
-    // Fallback - Gmail Web compose مباشرةً مش inbox
-    final Uri gmailWebUri = Uri.parse(
-      'https://mail.google.com/mail/?view=cm&fs=1&to=$encodedEmail&su=$encodedSubject&body=$encodedBody',
-    );
+  final gmailWeb = Uri.parse(
+    'https://mail.google.com/mail/?view=cm&to=$email&su=$encodedSubject&body=$encodedBody',
+  );
 
-    if (await canLaunchUrl(gmailAppUri)) {
-      await launchUrl(gmailAppUri);
-    } else {
-      await launchUrl(gmailWebUri, webOnlyWindowName: '_blank');
-    }
+  final mailto = Uri.parse(
+    'mailto:$email?subject=$encodedSubject&body=$encodedBody',
+  );
+
+  if (await canLaunchUrl(gmailApp)) {
+    await launchUrl(gmailApp);
     return;
   }
 
-  if (Platform.isAndroid) {
-    // ✅ Android - يجرب Gmail App الأول
-    final Uri gmailAppUri = Uri.parse(
-      'googlegmail://co?to=$encodedEmail&subject=$encodedSubject&body=$encodedBody',
-    );
-
-    final Uri mailtoUri = Uri(
-      scheme: 'mailto',
-      path: email,
-      queryParameters: {'subject': subject, 'body': body},
-    );
-
-    if (await canLaunchUrl(gmailAppUri)) {
-      await launchUrl(gmailAppUri, mode: LaunchMode.externalApplication);
-    } else if (await canLaunchUrl(mailtoUri)) {
-      await launchUrl(mailtoUri, mode: LaunchMode.externalApplication);
-    } else {
-      throw Exception('No email client found');
-    }
+  if (await canLaunchUrl(gmailWeb)) {
+    await launchUrl(gmailWeb, mode: LaunchMode.externalApplication);
     return;
   }
 
-  if (Platform.isIOS) {
-    // ✅ iOS - يجرب Gmail App الأول
-    final Uri gmailAppUri = Uri.parse(
-      'googlegmail://co?to=$encodedEmail&subject=$encodedSubject&body=$encodedBody',
-    );
-
-    final Uri mailtoUri = Uri(
-      scheme: 'mailto',
-      path: email,
-      queryParameters: {'subject': subject, 'body': body},
-    );
-
-    if (await canLaunchUrl(gmailAppUri)) {
-      await launchUrl(gmailAppUri, mode: LaunchMode.externalApplication);
-    } else if (await canLaunchUrl(mailtoUri)) {
-      await launchUrl(mailtoUri, mode: LaunchMode.externalApplication);
-    } else {
-      throw Exception('No email client found');
-    }
-    return;
-  }
-
-  throw UnsupportedError('Platform not supported');
+  await launchUrl(mailto);
 }

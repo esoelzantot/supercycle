c98ffedf704c9adcdaf1c3f2030b9f84633fb55e
EsoElzantot <eslamelzantot@gmail.com>
2026-04-28 16:42:28 +0300

    update social channels data

--- lib/features/home/data/content/social_channels_data.dart
+++ lib/features/home/data/content/social_channels_data.dart
@@ -3,7 +3,7 @@ class Channels {
       'https://www.facebook.com/profile.php?id=61568988568446&rdid=cnpZJN0niv0LhRdK&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F18JXXkknQ7%2F%3Futm_source%3Dig%26utm_medium%3Dsocial%26utm_content%3Dlink_in_bio#';
   static String instagram =
       'https://www.instagram.com/supercycle.eg?fbclid=IwY2xjawRZjvNleHRuA2FlbQIxMABicmlkETFBMDF5aVNSRTdOUjB1NUcyc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHvEbVfy4gMji7YE695RAemJIkPi55IOFfUolnXX6VQetHYpjvGsjQ0VFA42H_aem_fDqI37Y-4vi-m8mVXu95gg';
-  static String whatsapp = '01228851029';
+  static String whatsapp = '+201228851029';
   static String linkedin = 'https://www.linkedin.com/in/super-cycle-0a9936403/';
   static String email = 'supercycle.eg@gmail.com';
 }
